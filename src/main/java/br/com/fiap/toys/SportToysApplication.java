ef32f78d5c825654245f734113c8ac4c2a0475ee
lucas <lucaswtds@gmail.com>
2025-09-02 22:10:06 -0300

    setup: with spring initializr

---
+++ src/main/java/br/com/fiap/toys/SportToysApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.toys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportToysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportToysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/toys/SportToysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.toys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportToysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
